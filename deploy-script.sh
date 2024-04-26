cfc8955acc08a2623fecf0a71fb40f7188ec0762
Kowsika Devi <144236845+kowsikadevi21082006@users.noreply.github.com>
2024-04-26 14:49:18 +0530

    Update deploy-script.sh

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,19 +1,21 @@
 # remove junk
+echo "STEP 1 : removing junk"
 cd ./backend/
 rm -rf /build 
 rm -rf /node_modules
 cd ..
 
 # build front end
+echo "STEP 2 : building front end"
 cd ./frontend
 npm install
 npm run build
-mv ./build ../backend/
+mv -vf ./build ../backend/
 
 # build backend
+echo "STEP 3 : building back end"
 cd ../backend
 npm install
 
-# start server
-# npm start
+echo "STEP 4 : BUILDING COMPLETE"
 
