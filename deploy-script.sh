09576ecac9b97794fc15edeffdf05dcee8f3160b
Kowsika Devi <144236845+kowsikadevi21082006@users.noreply.github.com>
2024-04-26 14:13:19 +0530

    Add files via upload

---
+++ deploy-script.sh
@@ -0,0 +1,19 @@
+# remove junk
+cd ./backend/
+rm -rf /build 
+rm -rf /node_modules
+cd ..
+
+# build front end
+cd ./frontend
+npm install
+npm run build
+mv ./build ../backend/
+
+# build backend
+cd ../backend
+npm install
+
+# start server
+npm start
+
